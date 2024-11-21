20db439ea9e7b5c21481b8caf63c6d43da95dd74
biplovsoftwarica <240148@softwarica.edu.np>
2024-11-21 10:37:25 +0545

    created views

--- textsummarizer/views.py
+++ textsummarizer/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import textForm
+from django.http import JsonResponse
+from .utils import ttsummarizer
 
-# Create your views here.
+
+
+def textsummarizerhome(request):
+    if request.method == 'POST':
+        form = textForm(request.POST)
+        if form.is_valid():
+            input_text = form.cleaned_data['input_text']
+            text = ttsummarizer(input_text)
+            return render(request, 'textsummarizer/textsummarizerhome.html', {'form': form, 'text': text})
+    else:
+        form = textForm()
+    return render(request, 'textsummarizer/textsummarizerhome.html', {'form': form})
